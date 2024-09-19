7c82ee1bb24dfbb896e4bd788041f4aac4c6c0fe
banidzakaria101 <banidzakaria2@gmail.com>
2024-09-19 15:01:15 +0100

    update login component

--- oline_fish_shope_frontend/src/app/app.module.ts
+++ oline_fish_shope_frontend/src/app/app.module.ts
@@ -3,14 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
 import { BoardAdminComponent } from './board-admin/board-admin.component';
 import { BoardUserComponent } from './board-user/board-user.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './authentication/login/login.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,11 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterOutlet,
+    RouterLink,
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ oline_fish_shope_frontend/src/app/authentication/login/login.component.ts
@@ -0,0 +1,84 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { JwtService } from '../../services/jwt.service';
+import { LoginUserDto } from '../../dto/login-user-dto.dto';
+import { LoginResponse } from '../../dto/login-response.model';
+import { Role } from '../../enums/role';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+    private jwtService: JwtService
+  ) {
+    this.loginForm = this.fb.group({
+      userNameOrEmail: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  login() {
+    if (!this.loginForm.valid) {
+      console.error('Form is not valid.');
+      return;
+    }
+
+    const formValues = this.loginForm.value;
+    const loginUser: LoginUserDto = {
+      userNameOrEmail: formValues.userNameOrEmail,
+      password: formValues.password
+    };
+
+    this.authService.authenticate(loginUser).subscribe({
+      next: (response : LoginResponse) => this.handleLoginSuccess(response),
+      error: (err) => this.handleLoginError(err),
+      complete: () => console.log('Login process complete')
+    })
+  }
+
+  private handleLoginSuccess(response: LoginResponse) {
+    console.log('login successful:', response);
+
+    const token = response?.token;
+    if(!token) {
+      console.error('No token found in the response!');
+      return;
+    }
+
+    console.log('Token expires in: ', response.expiresIn);
+
+    try {
+      const role: string | null = this.jwtService.getUserRole(token);
+      this.redirectUserByRole(role);
+    } catch (error) {
+      console.error('Token decoding fails:', error);
+    }
+  }
+
+  private handleLoginError(error: any) {
+    console.error('Login failed:', error);
+  }
+
+  private redirectUserByRole(role: string | null) {
+    switch(role) {
+      case Role.ADMIN.toString():
+        this.router.navigate(['/admin-dashboard']);
+        break;
+      case Role.CUSTOMER.toString():
+        this.router.navigate(['/user-dashboard']);
+        break;
+      default:
+        console.error('Unknown:', role)
+    }
+  }
+}
